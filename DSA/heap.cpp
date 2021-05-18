90dfb2c24ed5a51aa0346668c024319dc47471b6
Debojyoti1915001 <debojyoti76@gmail.com>
2021-05-18 20:10:41 +0530

    added heap

--- DSA/heap.cpp
+++ DSA/heap.cpp
@@ -10,15 +10,48 @@ void Insert(int A[], int n)
     }
     A[i] = temp;
 }
+void Delete(int A[], int n)
+{
+    int x, i, j;
+    x = A[n];
+    A[1] = A[n];
+    i = 1, j = 2 * i;
+    while (j < n - 1)
+    {
+        if (A[j + 1] > A[j])
+            j = j + 1;
+        if (A[i] < A[j])
+        {
+            swap(A[i], A[j]);
+            i = j;
+            j = 2 * j;
+        }
+        else
+        {
+            break;
+        }
+    }
+    A[n] = x;
+}
 int main()
 {
     int A[] = {0, 10, 20, 30, 25, 5, 40, 35};
     int i;
-    for (i = 1; i <= 7; i++)
+    for (i = 2; i <= 7; i++)
     {
         Insert(A, i);
     }
-    for (i = 0; i <= 7; i++)
+    for (i = 1; i <= 7; i++)
+    {
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    for (i = 7; i >= 1; i--)
+    {
+        Delete(A, i);
+    }
+    // Delete(A, 7);
+    for (i = 1; i <= 7; i++)
     {
         cout << A[i] << " ";
     }
