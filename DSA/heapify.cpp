44029ca45909a2d0de8794391eaa5f582140305e
Debojyoti1915001 <debojyoti76@gmail.com>
2021-05-18 21:13:01 +0530

    added heap

---
+++ DSA/heapify.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    if (l < n && arr[largest] < arr[l])
+        largest = l;
+    if (r < n && arr[largest] < arr[r])
+        largest = r;
+    if (i != largest)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i); //since the leafs are always in a leaf
+    for (int i = n - 1; i > 0; i--)
+    {
+        swap(arr[0], arr[i]);
+        heapify(arr, i, 0);
+    }
+}
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cout << arr[i] << " ";
+    cout << "\n";
+}
+
+int main()
+{
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    heapSort(arr, n);
+    printArray(arr, n);
+}
