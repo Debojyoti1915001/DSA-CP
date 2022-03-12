fefc34d9c628ea3df47c77b8b0ac9a071972ea7d
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-03-12 12:47:46 +0530

    Time: 352 ms (27.65%), Space: 9.1 MB (61.27%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -6,6 +6,7 @@ public:
         dp[1]=1;
         for(int i=2;i<=n;i++){
             for(int j=1;j*j<=i;j++){
+                //reduce the square of j and add 1 cause you are adding a new element and find minimum among them
                 dp[i]=min(dp[i],dp[i-j*j]+1);
             }
         }
