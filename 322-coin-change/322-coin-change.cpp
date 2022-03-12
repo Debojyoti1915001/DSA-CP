e2b533c6dd71f756ce4a1e3e2cc24e102c54d700
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-03-12 11:38:46 +0530

    Time: 178 ms (23.55%), Space: 49.1 MB (12.96%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int coinChange(vector<int>& v, int m) {
+        int n=v.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1));
+        //for money if no of coins is zero
+        for(int j=1;j<=m;j++){
+            dp[0][j]=INT_MAX-1;
+        }
+        for(int j=1;j<=m;j++){
+            if(j%v[0]==0){
+                dp[1][j]=j/v[0];
+            }else{
+                dp[1][j]=INT_MAX-1;
+            }
+        }
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(j>=v[i-1]){
+                    //dp[i][j-v[i-1]]+1 means we are adding one more coin of type v[i-1] 
+                    // here we are not taking i-1 cause we can add more coins of type v[i-1]
+                    //dp[i-1][j]  means we are not adding that coin
+                    dp[i][j]=min(dp[i][j-v[i-1]]+1,dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][m]==INT_MAX-1?-1:dp[n][m];
+    }
+};
