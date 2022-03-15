1b9f1a9f98584a5fc35ad0f5683c74bc0b80cfc9
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-03-15 12:44:46 +0530

    Time: 8 ms (90.05%), Space: 9.6 MB (41.53%) - LeetHub

---
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> matrixBlockSum(vector<vector<int> >& mat, int k) {
+        int m=mat.size(),n=mat[0].size();
+        vector<vector<int> > dp(m+1,vector<int>(n+1));
+        dp[0][0]=mat[0][0];
+        for(int i=1;i<n;i++){
+            dp[0][i]=dp[0][i-1]+mat[0][i];
+        }
+        for(int i=1;i<m;i++){
+            dp[i][0]=dp[i-1][0]+mat[i][0];
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1]+mat[i][j];
+            }
+        }
+        vector<vector<int> > ans(m,vector<int>(n));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                //tl tr
+                //bl br
+                
+                int tl=(((j-k-1)>=0 && (i-k-1)>=0) ? dp[i-k-1][j-k-1] : 0);
+                int br=dp[min(i+k,m-1)][min(j+k,n-1)];
+                int bl=((j-k-1)>=0 ? dp[min(i+k,m-1)][j-k-1] : 0);
+                int tr=((i-k-1)>=0 ? dp[i-k-1][min(n-1,j+k)] : 0);
+                ans[i][j]=br-tr-bl+tl;
+            }
+        }
+        return ans;
+    }
+};
