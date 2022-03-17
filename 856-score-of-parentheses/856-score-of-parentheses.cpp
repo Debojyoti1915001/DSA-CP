f0fdc3c19257e1099832657722465eded7600b1a
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-03-17 11:05:53 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.12%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.cpp
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         stack<int>st;
-        int open=-1;
+        int open=-1;//used as an alternative of '(' since we are using an stack of integer
         for(auto c:s){
+            //if opening bracket insert -1
             if(c=='(')st.push(-1);
+            //else if you get a closing bracket back we push 1
             else if(c==')'){
                 if(st.top()==-1){
                     st.pop();
                     st.push(1);
+                //or add the values in the stack till we get a opening bracket
                 }else{
                     int val=0;
                     while(st.top()!=-1){
@@ -16,6 +19,7 @@ public:
                         st.pop();
                     }
                     st.pop();
+                    //don't forget to double the  value 
                     val=val*2;
                     st.push(val);
                 }
