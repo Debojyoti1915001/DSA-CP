b2b9f995d8426130316b7080f6fff139e9ecc8c9
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-05-20 12:25:50 +0530

    Added solution - LeetHub

--- Common elements - GFG/common-elements.cpp
+++ Common elements - GFG/common-elements.cpp
@@ -10,23 +10,23 @@ class Solution
        vector <int> commonElements (int A[], int B[], int C[], int n1, int n2, int n3)
         {
             //code here.
-            vector <int>res1,res;
-            int i=0,j=0;
+            vector <int>res;
+            int i=0,j=0,k=0;
             while(i<n1&&j<n2){
-                if(A[i]==B[j]){res1.push_back(A[i]);i++;j++;}
+                if(A[i]==B[j]){A[k++]=A[i];i++;j++;}
                 else if(A[i]<B[j])i++;
                 else j++;
             }
             i=0,j=0;
-            while(i<res1.size()&&j<n3){
-                if(res1[i]==C[j]){
+            while(i<k&&j<n3){
+                if(A[i]==C[j]){
                     res.push_back(C[j]);
                     i++;
                     j++;
-                    while(i<res1.size()&&res1[i]==res1[i-1])i++;
+                    while(i<k&&A[i]==A[i-1])i++;
                     while(j<n3&&C[j]==C[j-1])j++;
                 }
-                else if(res1[i]<C[j])i++;
+                else if(A[i]<C[j])i++;
                 else j++;
             }
             return res;
