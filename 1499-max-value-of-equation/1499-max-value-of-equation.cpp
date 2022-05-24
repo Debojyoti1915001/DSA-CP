23423cdd670ddd2c5ad82e9abde26dd63c41c66c
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-05-24 12:01:35 +0530

    Time: 316 ms (82.19%), Space: 92.4 MB (46.72%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -4,6 +4,11 @@ public:
         int ans=INT_MIN;
         //see modtherfucker
         //since its sorted the equation becomes (yi-xi)+(yj+xj)
+//         Suppose i < j, then we can translate yi + yj + |xi - xj| to (yi - xi) + (yj + xj).
+
+// For a given point_j, since (yj + xj) is fixed, we only need to maximize the (yi - xi) among the previously seen point_i.
+
+// What data structure to use to efficiently find the biggest previously yi - xi for each point_j = (xj, yj)? MaxHeap!
         int n=pts.size();
         priority_queue<pair<int,int>>pq;
         for(int i=0;i<n;i++){
