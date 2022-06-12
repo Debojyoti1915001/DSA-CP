b9872975c5534d2e9b9fe8a8dba908c4bc731911
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-06-12 17:35:49 +0530

    Time: 11 ms (73.54%), Space: 7.7 MB (68.91%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         vector<int>dp(n);
-        dp[0]=1;
         int c1=0,c2=0,c3=0;
+        dp[0]=1;
         for(int i=1;i<n;i++){
+            //since only factors of 2,3,5 are considered
             dp[i]=min({2*dp[c1],3*dp[c2],5*dp[c3]});
             if(dp[i]==2*dp[c1])c1++;
             if(dp[i]==3*dp[c2])c2++;
