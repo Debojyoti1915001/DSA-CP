49d1507ae73f66ba4a88c627107309a77327eda0
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-06-12 13:22:40 +0530

    Time: 160 ms (5.03%), Space: 29.1 MB (9.86%) - LeetHub

--- 857-minimum-cost-to-hire-k-workers/857-minimum-cost-to-hire-k-workers.cpp
+++ 857-minimum-cost-to-hire-k-workers/857-minimum-cost-to-hire-k-workers.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
+    //think of k elements all together
     double mincostToHireWorkers(vector<int>& q, vector<int>& w, int k) {
         vector<vector<double>>workers;
+        //W[i]/Q[i]=W[j]/Q[j]
+        
+//         So to minimize the total wage, we want a small ratio.
+// So we sort all workers with their expected ratio, and pick up K first worker.
+// Now we have a minimum possible ratio for K worker and we their total quality.
+
+// As we pick up next worker with bigger ratio, we increase the ratio for whole group.
+// Meanwhile we remove a worker with highest quality so that we keep K workers in the group.
+// We calculate the current ratio * total quality = total wage for this group.
+
+// We redo the process and we can find the minimum total wage.
+// Because workers are sorted by ratio of wage/quality.
+// For every ratio, we find the minimum possible total quality of K workers.
         for(int i=0;i<q.size();++i){
             workers.push_back({(double)(w[i])/q[i],(double)q[i]});
         }
