6ce946cf9f82dc979dda53d0cf665a620f0f21b4
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-06-13 17:59:26 +0530

    Time: 302 ms (87.18%), Space: 90.7 MB (95.99%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    //time O(n) space O(n)
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         int res=0;
         for(int i=0;i<n;i++){
@@ -7,9 +8,13 @@ public:
         }
         return res;
     }
-    int dfs(vector<int>& manager, vector<int>& informTime,int cur){
+    int dfs(vector<int>& manager,vector<int>& informTime,int cur){
         if(manager[cur]!=-1){
+            //once we reach a non head node
+            //we find the sum of inform time thats above the current element
+            //and go onto adding them
             informTime[cur]+=dfs(manager,informTime,manager[cur]);
+            //since we have calulated when xyz was the manager we don't need to calculate it again
             manager[cur]=-1;
         }
         return informTime[cur];
