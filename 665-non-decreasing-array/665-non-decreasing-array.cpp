b44efa87c6677bc5ff3beb4ff4204056fb3fd9a7
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-06-25 13:15:00 +0530

    Time: 34 ms (73.11%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -5,7 +5,11 @@ public:
         for(int i=1;i<nums.size();i++){
             if(nums[i-1]>nums[i]){//then there is an issue
                 cnt++;
+                //       |
+                //3 6->4 4 
+                //when nums[i-2] is less than current->i then we have to increase current since nums[i-1]>nums[i]
                 if(i==1||nums[i-2]<=nums[i])nums[i-1]=nums[i];
+                //else you have to change nums[i] 
                 else nums[i]=nums[i-1];
             }
         }
