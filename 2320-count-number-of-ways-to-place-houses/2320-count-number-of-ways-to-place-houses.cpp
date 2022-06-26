32ad5dbcd322e4fad788b15b2ba6ec618a3eb1a8
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-06-27 01:49:59 +0530

    Time: 11 ms (100.00%), Space: 13 MB (50.00%) - LeetHub

---
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countHousePlacements(int n) {
+        vector<long long>dp(n+10);
+        dp[1]=2;
+        dp[2]=3;
+        int mod=1e9+7;
+        for(int i=3;i<=n;i++){
+            dp[i]=(dp[i-1]+dp[i-2])%mod;
+        }
+        return dp[n] * dp[n]%mod;
+    }
+};
