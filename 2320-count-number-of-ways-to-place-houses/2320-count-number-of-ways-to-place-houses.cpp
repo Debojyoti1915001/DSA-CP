285e746ab0f73cdabd6285f3866d46af87430b3a
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-06-27 01:52:14 +0530

    Time: 12 ms (100.00%), Space: 13.1 MB (50.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.cpp
@@ -6,6 +6,7 @@ public:
         dp[2]=3;
         int mod=1e9+7;
         for(int i=3;i<=n;i++){
+            //for road if it is in one way
             dp[i]=(dp[i-1]+dp[i-2])%mod;
         }
         return dp[n] * dp[n]%mod;
