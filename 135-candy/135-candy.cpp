fc7e15bb7edc78a182948f77828f9725aec3a0c2
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-07-04 20:00:07 +0530

    Time: 33 ms (52.60%), Space: 18.2 MB (23.59%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -4,11 +4,14 @@ public:
         int n=v.size(),ans=0;
         vector<int> left(n,1),right(n,1);
         for(int i=1;i<n;i++){
+            //if the next element is greater add one in left
             if(v[i]>v[i-1])left[i]=1+left[i-1];
         }
         for(int i=n-2;i>=0;i--){
+            //if the prev element is greater add one in right
             if(v[i+1]<v[i])right[i]=1+right[i+1];
         }
+        //get the max of left or right added to ans
         for(int i=0;i<n;i++)ans+=max(left[i],right[i]);
         return ans;
     }
