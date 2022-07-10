37aac0cc94d205f7e828cc9ce6d14d1506593dad
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-07-10 12:59:43 +0530

    Time: 231 ms (79.94%), Space: 90.5 MB (50.99%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int maxResult(vector<int>& nums, int k)
-    {
-        long curr=0;
-        deque<int> dq;     		// Monoqueue
-		
-		// Start answering the best possible results for index `i` from the back
-        for(int i=nums.size()-1;i>=0;i--)
-        {
-            curr=nums[i]+(dq.empty()?0:nums[dq.front()]);       // Get current best, the maximum result will be the first element in the monoqueue.
-			
-			// We remove all the smaller results greedily as they wont get used anyways (This helps the monoqueue to remain sorted)
-            while(!dq.empty()&&curr>nums[dq.back()])
-                dq.pop_back();	
-            dq.push_back(i);              // Insert current index into the monoqueue
-			
-			// Erase all the indices in deque that are greater than or equal to i+k.
-            if(dq.front()>=i+k)
-                dq.pop_front();
-            nums[i]=curr;               // Use input array as auxillary array to store the best results.
+    int maxResult(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>>pq;
+        int n=nums.size();
+        int cur=0;
+        for(int i=n-1;i>=0;i--){
+            while(pq.size()&&pq.top().second>i+k)pq.pop();
+            cur=nums[i]+(pq.size()==0?0:pq.top().first);
+            pq.push({cur,i});
+           
         }
-        return curr;                           // `curr` would have the best or the maxium result to reach end from index 0.
+        return cur;
     }
-    
 };
