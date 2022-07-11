dd31fc7527d6104f957f92ee6e5d3c6e993803cc
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-07-11 09:17:39 +0530

    Time: 15 ms (82.44%), Space: 29.9 MB (6.37%) - LeetHub

--- 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
+++ 889-construct-binary-tree-from-preorder-and-postorder-traversal/889-construct-binary-tree-from-preorder-and-postorder-traversal.cpp
@@ -9,16 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// root][......left......][...right..]  ---pre
+// [......left......][...right..][root]  ---post
 class Solution {
 public:
-        int preIndex = 0, posIndex = 0;
-    TreeNode* constructFromPrePost(vector<int>& pre, vector<int>& post) {
-        TreeNode* root = new TreeNode(pre[preIndex++]);
-        if (root->val != post[posIndex])
-            root->left = constructFromPrePost(pre, post);
-        if (root->val != post[posIndex])
-            root->right = constructFromPrePost(pre, post);
-        posIndex++;
-        return root;
+    unordered_map<int,int>m;
+    TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
+        int len=postorder.size();
+        for(int i=0;i<len;i++)m[postorder[i]]=i;
+        return construct(preorder,postorder,0,len-1,0,len-1);
+    }
+    TreeNode*construct(vector<int>pre,vector<int>post,int a,int b,int c,int d){
+        TreeNode*n=new TreeNode(pre[a]);
+        if(a==b)return n;
+        int t=pre[a+1];
+        int idx=m[t];
+        int len=idx-c+1;
+        n->left=construct(pre,post,a+1,a+len,c,c+len-1);
+        if(idx+1==d)return n;
+        n->right=construct(pre,post,a+len+1,b,idx+1,d-1);
+        return n;
     }
 };
