dea811805e014e2bcfc23a0c6fb5d55a3834181f
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-07-13 13:47:28 +0530

    Time: 3 ms (99.82%), Space: 13.4 MB (6.29%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        string s=tokens.back();
+        tokens.pop_back();
+        if(s!="+"&&s!="-"&&s!="/"&&s!="*")return stoi(s);
+        int r2=evalRPN(tokens);
+        int r1=evalRPN(tokens);
+        if(s=="+")return r1+r2;
+        if(s=="-")return r1-r2;
+        if(s=="/")return r1/r2;
+        return r1*r2;
+    }
+};
