f344f810ac4b237922a2f0f4e0c61f82eccc85ef
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-07-27 23:33:16 +0530

    Time: 65 ms (81.04%), Space: 9.7 MB (75.70%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -1,28 +1,33 @@
 class Solution {
 public:
-    int dp[50][50][50];
-    int solution(vector<vector<int>>& grid,int r1,int c1,int r2){
-        int n=grid.size();
+    int n;
+    int dp[55][55][55];
+    int solve(vector<vector<int>>& g,int r1,int c1,int r2){
         int c2=r1+c1-r2;
-        if(r1>=n||c1>=n||r2>=n||c2>=n||grid[r1][c1]==-1||grid[r2][c2]==-1)return INT_MIN;
+        if(r1>=n||r2>=n||c1>=n||c2>=n||g[r1][c1]==-1||g[r2][c2]==-1)return INT_MIN;
         if(dp[r1][c1][r2]!=-1)return dp[r1][c1][r2];
-        int cherries=0;
-        if(r1==n-1&&c1==n-1)return grid[r1][c1];
+        if(r1==n-1&&c1==n-1)return g[r1][c1];
+        int c=0;
         if(r1==r2&&c1==c2){
-            cherries+=grid[r1][c1];
+            c=g[r1][c1];
         }else{
-            cherries+=grid[r1][c1]+grid[r2][c2];
+            c=g[r2][c2]+g[r1][c1];
         }
-        int f1=solution(grid,r1+1,c1,r2+1);
-        int f2=solution(grid,r1,c1+1,r2);
-        int f3=solution(grid,r1+1,c1,r2);
-        int f4=solution(grid,r1,c1+1,r2+1);
-        cherries+=max({f1,f2,f3,f4});
-        return dp[r1][c1][r2]=cherries;
+        if(dp[r1+1][c1][r2+1]==-1)
+        dp[r1+1][c1][r2+1]=solve(g,r1+1,c1,r2+1);
+        if(dp[r1][c1+1][r2+1]==-1)
+        dp[r1][c1+1][r2+1]=solve(g,r1,c1+1,r2+1);
+        if(dp[r1+1][c1][r2]==-1)
+        dp[r1+1][c1][r2]=solve(g,r1+1,c1,r2);
+        if(dp[r1][c1+1][r2]==-1)
+        dp[r1][c1+1][r2]=solve(g,r1,c1+1,r2);
+        int mx=max({dp[r1+1][c1][r2+1],dp[r1][c1+1][r2+1],dp[r1+1][c1][r2],dp[r1][c1+1][r2]});
+        c+=mx;
+        return dp[r1][c1][r2]=c;
     }
-    int cherryPickup(vector<vector<int>>& grid) {
-        int n=grid.size();
+    int cherryPickup(vector<vector<int>>& g) {
+        n=g.size();
         memset(dp,-1,sizeof(dp));
-        return max(0,solution(grid,0,0,0));
+        return max(0,solve(g,0,0,0));
     }
 };
