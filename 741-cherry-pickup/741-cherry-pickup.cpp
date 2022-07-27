00f53571622e03d2e4fa0acd3d5fc6dbca66e5bf
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-07-27 23:39:52 +0530

    Time: 929 ms (14.14%), Space: 63.5 MB (15.75%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int n;
     int dp[55][55][55];
+    vector<vector<int>>dirs{{1,0,1},{0,1,1},{1,0,0},{0,1,0}};
     int solve(vector<vector<int>>& g,int r1,int c1,int r2){
         int c2=r1+c1-r2;
         if(r1>=n||r2>=n||c1>=n||c2>=n||g[r1][c1]==-1||g[r2][c2]==-1)return INT_MIN;
@@ -13,16 +14,13 @@ public:
         }else{
             c=g[r2][c2]+g[r1][c1];
         }
-        if(dp[r1+1][c1][r2+1]==-1)
-        dp[r1+1][c1][r2+1]=solve(g,r1+1,c1,r2+1);
-        if(dp[r1][c1+1][r2+1]==-1)
-        dp[r1][c1+1][r2+1]=solve(g,r1,c1+1,r2+1);
-        if(dp[r1+1][c1][r2]==-1)
-        dp[r1+1][c1][r2]=solve(g,r1+1,c1,r2);
-        if(dp[r1][c1+1][r2]==-1)
-        dp[r1][c1+1][r2]=solve(g,r1,c1+1,r2);
-        int mx=max({dp[r1+1][c1][r2+1],dp[r1][c1+1][r2+1],dp[r1+1][c1][r2],dp[r1][c1+1][r2]});
-        c+=mx;
+        int further=INT_MIN;
+        for(auto i:dirs){
+            if(dp[r1+i[0]][c1+i[1]][r2+i[2]]==-1)
+            dp[r1+i[0]][c1+i[1]][r2+i[2]]=solve(g,r1+i[0],c1+i[1],r2+i[2]);
+            further=max(further,dp[r1+i[0]][c1+i[1]][r2+i[2]]);
+        }
+        c=c+further;
         return dp[r1][c1][r2]=c;
     }
     int cherryPickup(vector<vector<int>>& g) {
