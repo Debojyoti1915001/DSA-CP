ad8692c0d4bd960441b1203f9296edd8f9ee7eaa
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-08-01 02:13:44 +0530

    Time: 16 ms (61.16%), Space: 7.7 MB (66.73%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int nthUglyNumber(int n) {
         vector<int>dp(n);
-        int c1=0,c2=0,c3=0;
+        int c1=0,c2=0,c3=0;//we contain 3 pointers all of which points to 0 initially
         dp[0]=1;
         for(int i=1;i<n;i++){
             //since only factors of 2,3,5 are considered
