57ab9b40494b3e1226fa0e9e19a82f125cb4b2e6
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-08-04 20:31:45 +0530

    Time: 149 ms (60.00%), Space: 5.4 MB (100.00%) - LeetHub

---
+++ 526-beautiful-arrangement/526-beautiful-arrangement.c
@@ -0,0 +1,28 @@
+int ans=0;
+void dfs(int arr[],int m[],int n,int idx){
+    if(idx==n){
+        for(int i=0;i<=n;i++){
+            if(i==n)ans++;
+            else if(arr[i]%(i+1)!=0&&(i+1)%arr[i]!=0){
+                return;
+            }
+        }
+        return;
+    }
+    for(int i=1;i<=n;i++){
+        if(m[i]==0){
+            if((idx+1)%(i)!=0&&i%(idx+1)!=0)continue;
+            m[i]=1;
+            arr[idx]=i;
+            dfs(arr,m,n,idx+1);
+            m[i]=0;
+        }
+    }
+}
+int countArrangement(int n){
+    ans=0;
+    int arr[30],m[30]={};
+    if(n==1)return 1;
+    dfs(arr,m,n,0);
+    return ans;
+}
