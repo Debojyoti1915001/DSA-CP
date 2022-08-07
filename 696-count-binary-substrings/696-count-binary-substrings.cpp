a91892a4278c4780b29741587eb498e23cab8642
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-08-07 16:28:14 +0530

    Time: 35 ms (43.58%), Space: 10.4 MB (80.76%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.cpp
+++ 696-count-binary-substrings/696-count-binary-substrings.cpp
@@ -3,23 +3,19 @@ public:
     int countBinarySubstrings(string s) {
         int n=s.size();
         int ans=0;
-        int c1=0;
-        int c0=0;    
-        for(int i=0;i<n;i++){
-            if(s[i]=='0'){
-                c0=0;
-                c0++;
-                while(i<s.size()&&s[i]==s[i+1])i++,c0++;
-                ans+=min(c1,c0);
-            }
-            if(s[i]=='1'){
-                c1=0;
-                c1++;
-                while(i<s.size()&&s[i]==s[i+1])i++,c1++;
-                ans+=min(c1,c0);
+        int prev=0;
+        int cur=1;    
+        for(int i=1;i<n;i++){
+            if(s[i]==s[i-1]){
+                cur++;
+            }else{
+                ans+=min(cur,prev);
+                prev=cur;
+                cur=1;
             }
             
         }
+        ans+=min(cur,prev);
         return ans;
     }
 };
