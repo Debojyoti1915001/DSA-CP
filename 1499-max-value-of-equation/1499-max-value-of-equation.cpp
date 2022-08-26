91843445840489fb7f4a7abb22b0c0a5d2023aa4
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-08-26 18:18:14 +0530

    Time: 788 ms (6.79%), Space: 104.1 MB (15.34%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.cpp
+++ 1499-max-value-of-equation/1499-max-value-of-equation.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
-    int findMaxValueOfEquation(vector<vector<int>>& pts, int k) {
-        int ans=INT_MIN;
-        //see modtherfucker
-        //since its sorted the equation becomes (yi-xi)+(yj+xj)
-//         Suppose i < j, then we can translate yi + yj + |xi - xj| to (yi - xi) + (yj + xj).
-
-// For a given point_j, since (yj + xj) is fixed, we only need to maximize the (yi - xi) among the previously seen point_i.
-
-// What data structure to use to efficiently find the biggest previously yi - xi for each point_j = (xj, yj)? MaxHeap!
-        int n=pts.size();
+    int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
         priority_queue<pair<int,int>>pq;
-        for(int i=0;i<n;i++){
-            while(!pq.empty()&&pts[i][0]-pq.top().second>k)pq.pop();
-            if(!pq.empty())ans=max(ans,pq.top().first+pts[i][0]+pts[i][1]);
-            pq.push({pts[i][1]-pts[i][0],pts[i][0]});
+        int ans=INT_MIN;
+        for(auto p:points){
+            while(pq.size()&&p[0]-pq.top().second>k)pq.pop();
+            if(pq.size())ans=max(ans,pq.top().first+p[0]+p[1]);
+            pq.push({p[1]-p[0],p[0]});
         }
         return ans;
     }
