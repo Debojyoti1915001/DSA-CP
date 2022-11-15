4209262dee224f7d022f8243d2a7cd6a26ba3144
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2022-11-15 11:46:58 +0530

    Time: 64 ms (55.74%), Space: 30.8 MB (93.85%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -12,7 +12,6 @@
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        if(!root)return 0;
-        return countNodes(root->left)+countNodes(root->right)+1;
+        return !root?0:countNodes(root->left)+countNodes(root->right)+1;
     }
 };
