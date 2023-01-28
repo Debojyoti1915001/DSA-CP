2883e9e5b40d079363134cd4c5753cdae9dcdc1f
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2023-01-28 22:56:59 +0530

    Time: 77 ms (56.47%), Space: 33.5 MB (54.90%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -0,0 +1,34 @@
+class SummaryRanges {
+public:
+ 
+  SummaryRanges() {
+        
+  }
+    
+  void addNum(int val) {
+    data_given.insert(val);
+  }
+    
+  vector<vector<int>> getIntervals() {
+      vector<vector<int>> res;
+      int start = -1;
+      int end = -1;
+      for ( auto  &item: data_given) {
+          if (start == -1) {
+              start = item;
+              end = item;
+          } else if (item == end + 1) {
+              end = item;
+          } else {
+              res.push_back({start, end});
+              start = end = item;
+          }
+      }
+      
+      if (start != -1) res.push_back({start, end});
+      
+      return res;
+  }
+private:
+  set<int> data_given;
+};
