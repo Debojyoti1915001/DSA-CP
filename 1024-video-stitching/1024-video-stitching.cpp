526d6e58a93d55320a9931e1cefc68f23b950cf8
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2023-06-04 00:59:16 +0530

    Time: 12 ms (6.87%), Space: 10.2 MB (9.25%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.cpp
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int videoStitching(vector<vector<int>>& clips, int time) {
-        vector<int>dp(time+2,time+1);
+        vector<int>dp(time+2,1e9);
         dp[0]=0;
         for(int i=0;i<=time;i++){
             for(auto clip:clips){
@@ -9,7 +9,7 @@ public:
                     dp[i]=min(dp[i],dp[clip[0]]+1);
                 }
             }
-            if(dp[i]>=time+1)return -1;
+            if(dp[i]==1e9)return -1;
         }
         return dp[time];
     }
