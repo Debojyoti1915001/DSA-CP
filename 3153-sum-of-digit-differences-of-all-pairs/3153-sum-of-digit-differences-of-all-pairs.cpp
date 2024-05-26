31438e886416ccbdd6dd3ac936d0975a462267e8
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2024-05-27 01:39:43 +0530

    Time: 132 ms (62.16%), Space: 104.5 MB (48.15%) - LeetHub

---
+++ 3153-sum-of-digit-differences-of-all-pairs/3153-sum-of-digit-differences-of-all-pairs.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long sumDigitDifferences(vector<int>& nums) {
+        vector<vector<int>>mp(to_string(nums[0]).size(),vector<int>(10));
+        for(auto &i:nums){
+            string s=to_string(i);
+            for(int j=0;j<s.size();j++){
+                mp[j][s[j]-'0']++;
+            }
+        }
+        long long ans=0;
+        for(int i=0;i<to_string(nums[0]).size();i++){
+            for(int j=0;j<10;j++){
+                for(int k=j+1;k<10;k++){
+                    int cntj=mp[i][j];
+                    int cntk=mp[i][k];
+                    if(cntj==0||cntk==0)continue;
+                    ans+=(cntj*cntk);
+                }   
+            }
+        }
+        return ans;
+    }
+};
