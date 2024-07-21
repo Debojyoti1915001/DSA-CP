66de9a27f2fa0ea5a8412728afbc58966d44ffc8
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2024-07-21 20:51:38 +0530

    Time: 31 ms (50.00%), Space: 16 MB (16.67%) - LeetHub

--- 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
+++ 3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.cpp
@@ -2,8 +2,9 @@ class Solution {
 public:
     int maxOperations(string s) {
         int one=0,ans=0;
+        s.push_back('1');
         for(int i=0;i<s.size();i++){
-            if((i>0&&s[i-1]=='0'&&s[i]=='1')||(s[i]=='0'&&i==s.size()-1)){
+            if(i>0&&s[i-1]=='0'&&s[i]=='1'){
                 ans+=one;
             }
             if(s[i]=='1'){
