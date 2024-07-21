bdc96cd4ef78135c6578d1d7d0f2f6d50ee9b012
Debojyoti Das <62800419+Debojyoti1915001@users.noreply.github.com>
2024-07-21 20:22:20 +0530

    Time: 44 ms (100.00%), Space: 17.3 MB (20.00%) - LeetHub

--- 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3227-vowels-game-in-a-string.cpp
@@ -7,22 +7,11 @@ public:
         return false;
     }
     bool doesAliceWin(string s) {
-        int o1=0,o2=0,e=0;
-        int len=0;
-        for(int i=0;i<=s.size();i++){
-            if(i!=s.size()&&isVowel(s[i])){
-                len++;
-            }else if((i==s.size()&&len!=0)||len!=0){
-                if(len%2==0)e++;
-                else if(len==1){
-                    o1++;
-                }else{
-                    o2++;
-                }
-                len=0;
+        for(auto &i:s){
+            if(isVowel(i)){
+                return true;
             }
         }
-        if(o1==0&&o2==0&&e==0)return false;
-        return true;
+        return false;
     }
 };
